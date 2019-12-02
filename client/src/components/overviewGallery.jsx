38d8ce16da816467a18d929b0992381fb61f8621
elchangtw <elchang.tw@gmail.com>
2019-12-01 22:24:57 -0800

    adds sample api get request

--- client/src/components/overviewGallery.jsx
+++ client/src/components/overviewGallery.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
+const url = 'http://localhost:3000/';
+
 class OverviewGallery extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
     };
   }
+
+  componentDidMount () {
+    var randomLocationId = Math.floor(Math.random() * 100 + 1);
+    // fetch(url + 'overviewGallery/sample/' + randomLocationId, {
+    fetch(url + 'overviewGallery/sample/' + 5, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    .catch( err => {
+      console.error(err);
+    })
+    .then( result => {
+      return result.json();
+    })
+    .then( result => {
+      console.log(result);
+      console.log(JSON.stringify(result));
+    });
+  }
   
-  render() {
+  render () {
     return (
       <div>
         <h1>Overview Gallery</h1>
--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const path = require('path');
+var controller = require('./controller.js');
+
 const app = express();
 
 app.set('port', 3000);
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-// GET /overviewgallery/:restaurantId
-
-app.get('/', (req, res) => { 
-    res.status(200).send('Hello World!');
+app.get('/overviewgallery/sample/:locationId', (req, res) => {
+  let locationId = req.params.locationId;
+  controller.sampleQuery(locationId)
+  .catch( err => {
+    res.status(404).send(err);
+  })
+  .then( result => {
+    res.status(200).send(result);
+  });
 });
 
 if (!module.parent) {
-    app.listen(app.get('port'));
-    console.log('Listening on:', app.get('port'));
+  app.listen(app.get('port'));
+  console.log('Listening on:', app.get('port'));
 }
