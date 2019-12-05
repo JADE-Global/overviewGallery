fcb727c5209c2c48cbee566125596fd16535799f
elchangtw <elchang.tw@gmail.com>
2019-12-05 12:17:02 -0800

    refactors carousel to class component

--- client/src/components/carousel/carousel.jsx
+++ client/src/components/carousel/carousel.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import styles from './carousel.css';
 
-function Carousel (props) {
-  const imageList = props.images.map((image) => {
-    return (
-      <span key={image.id} id={`#carouselImg-${image.id}`} onClick={() => props.clickHandler(image.id)}>
-        <img src={image.img_url} className={styles.img_style} />
-      </span>
-    );
-  });
- 
-  return (
-    <div className={styles.container}>{imageList}</div>
-  );
-}
+class Carousel extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      position: 0,
+    };
+  }
   
+  render () {
+    const imageList = this.props.images.map((image) => {
+      return (
+        <span key={image.id} id={`#carouselImg-${image.id}`} onClick={() => this.props.clickHandler(image.id)}>
+          <img src={image.img_url} className={styles.img_style} />
+        </span>
+      );
+    });
+
+    return (
+      <div className={styles.container}>
+        {imageList}
+      </div>
+    );
+  }
+}
+
 export default Carousel;
